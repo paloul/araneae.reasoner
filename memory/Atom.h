9af6b0f6c99f3fc74e5533c0263551db2c26eac4
George K. Paloulian <george.k.paloulian@gmail.com>
2021-08-14 13:13:15 -0700

    Added utils par-hashmap. Changes to cmake source inclusion

--- memory/Atom.h
+++ memory/Atom.h
@@ -5,4 +5,17 @@
 #ifndef ARANEAE_REASONER_ATOM_H
 #define ARANEAE_REASONER_ATOM_H
 
+#include <string>
+#include <parallel_hashmap/phmap.h>
+
+using phmap::flat_hash_map;
+
+namespace araneae {
+
+    struct Atom {
+        std::string label;
+    };
+
+}
+
 #endif //ARANEAE_REASONER_ATOM_H
