d9ad3294d03abfc7d2510738a3775ee19aefe8f0
George K. Paloulian <george.k.paloulian@gmail.com>
2021-08-31 18:09:57 -0700

    Removed c++ files and cmake build

--- memory/Memory.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Kev Paloulian on 7/31/2021.
-//
-
-#ifndef ARANEAE_REASONER_MEMORY_H
-#define ARANEAE_REASONER_MEMORY_H
-
-#endif //ARANEAE_REASONER_MEMORY_H
--- memory/Term.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Kev Paloulian on 7/31/2021.
-//
-
-#ifndef ARANEAE_REASONER_TERM_H
-#define ARANEAE_REASONER_TERM_H
-
-#endif //ARANEAE_REASONER_TERM_H
