4747df1ce3f2fca32586cafac6d2e9044e1cab63
George K. Paloulian <george.k.paloulian@gmail.com>
2021-12-22 17:02:48 -0800

    Main refactor

--- src/main/scala/paloul/araneae/reasoner/Main.scala
+++ src/main/scala/paloul/araneae/reasoner/Main.scala
@@ -1,50 +1,88 @@
 package paloul.araneae.reasoner
 
 import akka.actor.typed.ActorSystem
+import akka.http.scaladsl.Http
 import akka.management.cluster.bootstrap.ClusterBootstrap
 import akka.management.scaladsl.AkkaManagement
 import com.typesafe.config.ConfigFactory
 import paloul.araneae.reasoner.rest.RestServicesSupport
 
-object Main extends App with RestServicesSupport {
+import scala.concurrent.Future
 
-  // Load the application.conf file and create our own Settings helper class
-  val preConfig = ConfigFactory.load()
-  val settings: Settings = Settings(preConfig)
+object Main {
 
-  // If we are deploying to cloud then assign the canonical host name to the
-  // current IP address grabbed from the underlying OS support libs.
-  // When deploying to cloud and enabling cluster management and cluster bootstrap,
-  // the cluster seed nodes need to be cleared. The cluster seed nodes should only
-  // be used when you actually know the IP of seed nodes before the applications start.
-  // They are cleared so that cluster bootstrap can do its thing and discover nodes
-  // on the same network as peers.
-  def getRuntimeConfigMods: String = {
-    if (!settings.application.cloudDeploy) return ""
+  /**
+   * Based on Environment Variables defined in app.conf and loaded via Settings, dynamically
+   * generates a Config string required to work with the Cloud Deploy feature which starts up
+   * things like Akka Management and Akka Cluster Bootstrap/Discovery.
+   *
+   * @param cloudDeploy A bool from Settings that determines if deployment intended for cloud.
+   * @param configStr Initial String Config representation that will be concatenated with dynamic updates
+   * @return String containing config updates for Akka Remote & Management Hostnames and cleared Seed Nodes
+   */
+  def runtimeCloudDeployMods(cloudDeploy: Boolean, configStr: String = ""): String = {
+    // If we are deploying to cloud then assign the canonical host name to the
+    // current IP address grabbed from the underlying OS support libs.
+    // When deploying to cloud and enabling cluster management and cluster bootstrap,
+    // the cluster seed nodes need to be cleared. The cluster seed nodes should only
+    // be used when you actually know the IP of seed nodes before the applications start.
+    // They are cleared so that cluster bootstrap can do its thing and discover nodes
+    // on the same network as peers.
 
     // Get the current IP of the host
     val localhost: java.net.InetAddress = java.net.InetAddress.getLocalHost
     val localIpAddress: String = localhost.getHostAddress
 
-    "akka.remote.artery.canonical.hostname=" + localIpAddress + "\n" +
-      "akka.management.http.hostname=" + localIpAddress + "\n" +
-      "akka.cluster.seed-nodes = []" + "\n"
+    if (cloudDeploy)
+      configStr.concat(
+      s"""
+         |akka.remote.artery.canonical.hostname = $localIpAddress
+         |akka.management.http.hostname = $localIpAddress
+         |akka.cluster.seed-nodes = []
+         |""".stripMargin
+      )
+    else
+      configStr
   }
 
-  // Create new config by merging runtime live mods with the initial pre config loaded earlier
-  val config = ConfigFactory.load(ConfigFactory.parseString(getRuntimeConfigMods).withFallback(preConfig))
+  /**
+   * Generate Settings class that can be used throughout the application. Provides quick
+   * access to environment variables during runtime execution. Encapsulates Akka's Config class.
+   *
+   * @return A final representation of settings with all env vars and configs updated dynamically
+   */
+  def settings(): Settings = {
+    // Load the application.conf file and create our own Settings helper class
+    val preConfig = ConfigFactory.load() // pre config as some settings need to be updated during runtime
+    val preSettings: Settings = Settings(preConfig) // pre settings as we'll create new settings with updated config
 
-  // Root Behavior is the entry point for the whole akka cluster. It is the main behavior (actor)
-  // that all others spawn from, i.e. Cluster Sharding, Akka-Http APIs, etc.
-  // TODO: Define rootBehavior
+    // Final Settings from modded Config to reflect dynamic runtime updates. Settings encases Config in itself
+    Settings(
+      ConfigFactory.parseString(
+        runtimeCloudDeployMods(preSettings.application.cloudDeploy)).withFallback(preConfig))
+  }
 
-  // Start the Actor System
-  val system = ActorSystem[Nothing](rootBehavior, settings.application.akkaClusterName, config)
+  def startServices(system: ActorSystem[_], settings: Settings): Future[Http.ServerBinding] = {
 
-  // If we are deploying to cloud then start the management server and cluster bootstrap services
-  if (settings.application.cloudDeploy) {
-    AkkaManagement(system).start()
+  }
 
-    ClusterBootstrap(system).start()
+  def main(args: Array[String]): Unit = {
+
+    // Load the application.conf file and create our own Settings helper class
+    val settings: Settings = settings
+
+    // Root Behavior is the entry point for the whole akka cluster. It is the main behavior (actor)
+    // that all others spawn from, i.e. Cluster Sharding, Akka-Http APIs, etc.
+    // TODO: Define rootBehavior
+
+    // Start the Actor System
+    val system = ActorSystem[Nothing](rootBehavior, settings.application.akkaClusterName, config)
+
+    // If we are deploying to cloud then start the management server and cluster bootstrap services
+    if (settings.application.cloudDeploy) {
+      AkkaManagement(system).start()
+
+      ClusterBootstrap(system).start()
+    }
   }
 }
