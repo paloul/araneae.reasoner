e1982e65ad48ce783f19fd330b22507386704199
George K. Paloulian <george.k.paloulian@gmail.com>
2022-01-18 23:44:02 -0800

    Implemented apply() for DroneKafkaProcessor

--- src/main/scala/paloul/araneae/cluster/processors/DroneKafkaProcessor.scala
+++ src/main/scala/paloul/araneae/cluster/processors/DroneKafkaProcessor.scala
@@ -1,22 +1,52 @@
 package paloul.araneae.cluster.processors
 
-import akka.actor.typed.{ActorRef, Behavior}
+import akka.Done
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import paloul.araneae.cluster.actors.Drone
 import paloul.araneae.cluster.util.Settings
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.util.Try
 
 object DroneKafkaProcessor {
 
   //************************************************************************************
-  // Drone Messages
+  // DroneKafkaProcessor Messages
 
   /** Base Message type messages */
   sealed trait Command
+
+  // Internal notification message to self to signal consumer stopped
+  private case class KafkaConsumerStopped(reason: Try[Any]) extends Command
   //************************************************************************************
 
   def apply(shardRegion: ActorRef[Drone.Command], settings: Settings): Behavior[Nothing] = {
+    Behaviors
+      .setup[Command] { context =>
+        val result = startConsumingFromTopic(context.system, shardRegion, settings)
 
+        context.pipeToSelf(result) {
+          result => KafkaConsumerStopped(result)
+        }
+
+        Behaviors.receiveMessage[Command] {
+          case KafkaConsumerStopped(reason) =>
+            context.log.info("Consumer stopped {}", reason)
+            Behaviors.stopped
+        }
+
+      }.narrow
   }
 
-  // TODO: Implement me
+  private def startConsumingFromTopic(actorSystem: ActorSystem[_],
+                                      shardRegion: ActorRef[Drone.Command],
+                                      settings: Settings): Future[Done] = {
+
+    // TODO: Implement me
+
+  }
 
 }
