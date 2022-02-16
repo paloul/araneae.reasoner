1a0f0bf31bb20ed2587ed3e23578bfdac27a0ecb
George K. Paloulian <george.k.paloulian@gmail.com>
2022-02-15 22:37:18 -0800

    Tweaks to UUID class hierarchy

--- src/main/scala/paloul/araneae/reasoner/hypergraph/util/UUID.scala
+++ src/main/scala/paloul/araneae/reasoner/hypergraph/util/UUID.scala
@@ -1,5 +1,22 @@
 package paloul.araneae.reasoner.hypergraph.util
 
+/**
+ * Base Trait for all Araneae specific UUIDs.
+ * All specialized Levels of UUID implementations
+ * should extend from this trait
+ */
 trait UUID {
+
+  protected val uuid: java.util.UUID
+
+  def getLeastSignificantBits: Long = uuid.getLeastSignificantBits
+
+  def getMostSignificantBits: Long = uuid.getMostSignificantBits
+
+  override def toString: String = uuid.toString
+
+  override def hashCode(): Int = uuid.hashCode
+
+  override def equals(obj: Any): Boolean = uuid.equals(obj)
 
 }
