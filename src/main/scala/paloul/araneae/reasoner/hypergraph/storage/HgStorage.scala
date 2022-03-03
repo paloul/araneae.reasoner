5c7f1a3ec76c4c4eac1d7e56d01f7f763448f5ff
George K. Paloulian <george.k.paloulian@gmail.com>
2022-03-02 22:58:18 -0800

    Additional funcs to HgStorage trait

--- src/main/scala/paloul/araneae/reasoner/hypergraph/storage/HgStorage.scala
+++ src/main/scala/paloul/araneae/reasoner/hypergraph/storage/HgStorage.scala
@@ -51,4 +51,73 @@ trait HgStorage {
    */
   def store(handle: HgHandle, data: Array[Byte]): HgHandle
 
+  /**
+   * Remove a link value associated with the given <code>HgHandle</code> key
+   *
+   * @param handle A unique handle that refers to the link
+   * @return True if removed, false otherwise
+   */
+  def removeLink(handle: HgHandle): Boolean
+
+  /**
+   * Remove a raw data value associated with the given <code>HgHandle</code> key
+   *
+   * @param handle A unique handle that will be used as the key to the data
+   * @return True if removed, false otherwise
+   */
+  def removeData(handle: HgHandle): Boolean
+
+  /**
+   * Retrieve an existing link by its handle
+   *
+   * @param handle A unique handle that refers to the link
+   * @return An array of <code>HgHandle</code> that make up the link
+   */
+  def getLink(handle: HgHandle): Array[HgHandle]
+
+  /**
+   * Retrieve the raw data in bytes given the <code>HgHandle</code> as key
+   *
+   * @param handle A unique handle that will be used as the key to the data
+   * @return The data as array of bytes stored by the given handle key
+   */
+  def getData(handle: HgHandle): Array[Byte]
+
+  /**
+   * Check to see if the link defined by given <code>HgHandle</code> exists
+   *
+   * @param handle A unique handle that refers to the link
+   * @return True if it exists, false otherwise
+   */
+  def containsLink(handle: HgHandle): Boolean
+
+  /**
+   * Check to see if there is data bound to the given <code>HgHandle</code>
+   *
+   * @param handle A unique handle that will be used as the key to the data
+   * @return True if it exists, false otherwise
+   */
+  def containsData(handle: HgHandle): Boolean
+
+  /**
+   * Get an iterable list of other atom handles in the given atom's incidence set
+   *
+   * @param handle A unique handle to the atom we want to get incidence set of
+   * @return An iterable object over other <code>HgHandle</code>s
+   */
+  def getIncidenceResultSet(handle: HgHandle): Iterable[HgHandle]
+
+  /**
+   * Return the number of other atoms in the incidence set of given atom. In short,
+   * return the number of links pointing to the given atom defined by <code>HgHandle</code.
+   *
+   * @param handle A unique handle to the atom we want to get incidence set of
+   * @return Number of links as Long pointing to the given atom
+   */
+  def getIncidenceSetCardinality(handle: HgHandle): Long
+
+
+
+
+
 }
