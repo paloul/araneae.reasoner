e1eab0188a629be68915d802ec748bfe61b570c8
George K. Paloulian <george.k.paloulian@gmail.com>
2022-03-11 18:09:07 -0800

    Some additional background comment and explanation on what Incidence Set is

--- src/main/scala/paloul/araneae/reasoner/hypergraph/atom/util/HgIncidenceSet.scala
+++ src/main/scala/paloul/araneae/reasoner/hypergraph/atom/util/HgIncidenceSet.scala
@@ -3,8 +3,13 @@ package paloul.araneae.reasoner.hypergraph.atom.util
 import paloul.araneae.reasoner.hypergraph.handle.HgHandle
 
 /**
- * Represents an atom's incidence set. That is, a set containing all atoms pointing to a given
- * atom. Instances of this class can be cached and queried in memory.
+ * Represents an atom's incidence set. That is, a set containing all atoms
+ * pointing to a given atom. This might be confused with a link, I did.
+ * The incidence set does not define a (link) relationship. Its a basic
+ * collection of all other atoms that point to a particular/given atom.
+ * The collection of all other atoms that point to the particular/given
+ * atom can be across multiple unrelated links.
+ * Instances of this class can be cached and queried in memory.
  */
 sealed class HgIncidenceSet(val atom: HgHandle) extends HgAtomSet {
 
