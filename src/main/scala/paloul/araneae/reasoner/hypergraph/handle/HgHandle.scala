cb45783d05d5fcc57ad583c1d5c40ed93eb3fcb7
George K. Paloulian <george.k.paloulian@gmail.com>
2022-06-10 17:17:50 -0700

    Comments on HgHandle Moving things and renaming things in HgStorage

--- src/main/scala/paloul/araneae/reasoner/hypergraph/handle/HgHandle.scala
+++ src/main/scala/paloul/araneae/reasoner/hypergraph/handle/HgHandle.scala
@@ -2,6 +2,8 @@ package paloul.araneae.reasoner.hypergraph.handle
 
 /**
  * Represents a reference to a hypergraph atom. A handle holds system-level identity of an atom.
+ * A handle is only the unique identifier to an atom stored in memory, on disk, or wherever.
+ * The handle to an atom does not store any information about the atom, it is merely a pointer.
  */
 trait HgHandle {
 
